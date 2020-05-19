6bf4a01a1663301fdf98de1ca79c22d13b370549
SepSol <SR-75@live.com>
2020-05-19 15:22:37 +0430

    requiring mongoose model and env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+// our document imports:
+require('dotenv').config(); // we use this to store our sensitive information during development in environment variables
+require('./models/User'); // our mongoose document collection - schema model
 const express = require('express');
-const app = express();
-
+const authRouter = require('./routes/authentication');
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb+srv://admin:mypassword@track-server-db-ixu0w.mongodb.net/test?retryWrites=true&w=majority';
+
+
+const app = express();   // this creates our express app
+
+app.use(express.json()); // this parses requests to json format  -- this is called middleware
+app.use(authRouter);     // our request handler functions are here
+
+const mongoUri = process.env.MONGODB_ADMIN;
 
 // we add the options below so that we don't see any errors in our console.
 // mongoose === MongoClient
-mongoose.connect(mongoUri, {
+mongoose.connect (mongoUri, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
@@ -17,6 +26,8 @@ mongoose.connection.on('connected', () => console.log('Connected to mongo instan
 mongoose.connection.on('error', err => console.error('Error connecting to mongo.', err));
 
 app.get('/', (req, res) => res.send('Hello, world!'));
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
