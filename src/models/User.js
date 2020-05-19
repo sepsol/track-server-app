0aef291dd034202a8aaf93342e6d656b5f5ef051
SepSol <SR-75@live.com>
2020-05-20 02:42:43 +0430

    fixed a typo and made some adjustments to styling

--- src/models/User.js
+++ src/models/User.js
@@ -4,15 +4,8 @@ const bcrypt = require('bcrypt');
 
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true
-  },
-  password: {
-    type: String,
-    require: true
-  }
+  email:    { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
 
